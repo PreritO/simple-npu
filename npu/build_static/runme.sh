8cbe8cabdaa4851d85e6616aacffec3071b76450
PreritO <prerit.oberai@gmail.com>
2020-11-13 13:39:42 -0600

    working for sample_router.json file with small table.txt, latency = 1452 ns, 16 clusters, 8 cores/cluster

--- npu/build_static/runme.sh
+++ npu/build_static/runme.sh
@@ -29,15 +29,44 @@ rm *.csv
 # 02110-1301, USA.
 #
 
-vlvl="INVALID"
-echo $vlvl
-if [ ! $# -eq 0 ]
-then
-    echo "No args passed"
-    echo $#
-    vlvl=$1
-else
-    vlvl='minimal'
+if [ "$#" -ne 3 ]; then
+    echo "Invalid Args passed"
+    echo "Usage: $0 <verbosity level> <application: [nat, router, ecmp]> <debug_level: [none,debug]"
+    exit
 fi
-echo $vlvl
-./npu-sim -c Configs/ -Xp4 simple_router.json -Xtpop table.txt -Xin Configs/input.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl"
+vlvl="$1"
+application="$2"
+debug="$3"
+
+#echo $vlvl
+#./npu-sim -c Configs/ -Xp4 simple_router.json -Xtpop table.txt -Xin Configs/input.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+#./npu-sim -c Configs/ -Xp4 simple_router.json -Xtpop table-new.txt -Xin Configs/10kflows.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+#./npu-sim -c Configs/ -Xp4 simple_router.json -Xtpop router_table.txt -Xin Configs/10kflows.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+
+if [[ $debug == "debug" ]] ; then
+    if [[ $application == "nat" ]] ; then #NAT
+    #./npu-sim -c Configs/ -Xp4 apps/nat/nat_new.json -Xtpop apps/nat/nat_cmd_table.txt -Xin apps/pcaps/10kflows.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+        pfpdb npu-sim --args "-Xp4 apps/nat/nat_new.json -Xtpop apps/nat/nat_cmd_table.txt -v "$vlvl" -Xin apps/pcaps/10kflows.pcap -Xvalidation-out output.pcap" -v
+    elif [[ $application == "router" ]]; then
+        pfpdb npu-sim --args "-Xp4 simple_router.json -Xtpop apps/router/router_table.txt -v "$vlvl" -Xin apps/pcaps/170pkts.pcap -Xvalidation-out output.pcap" -v
+        #pfpdb npu-sim --args "-Xp4 simple_router.json -Xtpop table.txt -v "$vlvl" -Xin apps/pcaps/1kflows.pcap -Xvalidation-out output.pcap" -v
+        #pfpdb npu-sim --args "-Xp4 simple_router.json -Xtpop table.txt -v "$vlvl" -Xin Configs/input.pcap -Xvalidation-out output.pcap" -v
+    else
+        echo "not yet implemented.."
+        exit
+    fi
+
+else
+    if [[ $application == "nat" ]] ; then #NAT
+        ./npu-sim -c Configs/ -Xp4 apps/nat/nat_new.json -Xtpop apps/nat/nat_cmd_table.txt -Xin apps/pcaps/10kflows.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+    elif [[ $application == "router" ]]; then
+        ./npu-sim -c Configs/ -Xp4 simple_router.json -Xtpop table.txt -Xin Configs/input.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+        #./npu-sim -c Configs/ -Xp4 simple_router.json -Xtpop apps/router/router_table.txt -Xin apps/pcaps/10kflows.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+        #./npu-sim -c Configs/ -Xp4 simple_router.json -Xtpop apps/router/router_table.txt -Xin apps/pcaps/170pkts.pcap -Xvalidation-out reordered-output.pcap -v "$vlvl" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+
+    else
+        echo "not yet implemented.."
+        exit
+    fi
+
+fi
